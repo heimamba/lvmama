1949835129097d2983bb162f3164a6714e3edc98
xionger <xxqiang812@163.com>
2018-11-24 17:14:43 +0800

    第一次传入入

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    // 轮播图
+var mySwiper = new Swiper('.swiper-container', {
+   loop: true,
+
+   pagination: '.swiper-pagination',
+})  
+})
---
+++ mother_donkey/js/index.js
@@ -0,0 +1,10 @@
+﻿
+$(document).ready(function () {
+         // 轮播图
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+
+        // 如果需要分页器
+        pagination: '.swiper-pagination',
+    })  
+})
